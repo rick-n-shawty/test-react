399e2137f38b37db7bc27e186c65e5184a43f393
Sultan <sultanhoodie@gmail.com>
2023-01-27 11:40:45 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
+import axios from 'axios';
 function App() {
+  const [user, setUser] = useState({})
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+  const [secret, setSecret] = useState('')
+  const baseURL = 'http://localhost:5000'
+  axios.defaults.withCredentials = true
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+    try{
+      const res = await axios.post(baseURL + '/login', {name, password})
+      console.log(res)
+      const {accessToken} = res.data
+      setUser({accessToken: accessToken})
+    }catch(err){
+      setUser({})
+      console.log(err)
+    }
+  }
+  const getSecret = async () =>{
+    try{
+      const res = await axios.get(baseURL + '/secret', {headers: {
+        Authorization: `Bearer ${user.accessToken}`
+      }})
+      console.log(res)
+      const {msg} = res.data
+      setSecret(msg)
+    }catch(err){
+      console.log(err)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.accessToken ? 
+    <div>
+      <h1>U logged in</h1>
+      <h3>{secret}</h3>
+      <button onClick={getSecret}>get secret</button>
+    </div>
+    :
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+        placeholder='name'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        />
+        <input
+        placeholder='password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Log in</button>
+      </form>
+    </div>  
+    }
     </div>
   );
 }
